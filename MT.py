56bdd24c4051ab5614b3e8746794273d655bfdc7
Geovani29 <pgeovani@uninorte.edu.co>
2022-09-14 21:26:33 +0000

    commit

--- MT.py
+++ MT.py
@@ -1,18 +1,24 @@
 
 class cine:
-    def __init__(self, se) -> None:
-        self.se = se
+    def __init__(self, sele) -> None:
+        self.sele = sele
     
-    def selec_peli (self, pelicula):
+    def zona (self, pelicula):
         print (' Elija su pelicula: 1. Crepusculo, 2. Iron man, 3. Hulk')
-        se = input()
+        sele = input() #escogemos la pelicula
         
-        if se == 1:
+        if sele == 1:
             print ('horas disponible: 3 pm, 6 pm, 9 pm')
             print ('digite la hora a la que quiere ver su pelicula')
             hora = input ()
             while hora != 3 or hora != 6 or hora != 9:
                  print ('la hora no es valida, digitela de nuevo')
                  hora = input ()
+            posicion = (1, 100)
+            print ('elija su posicion')
+            posicion = input()
             
+            print('¿Cual es la cantidad de voletos que desea?')
+            boletos = input ()
+
         
